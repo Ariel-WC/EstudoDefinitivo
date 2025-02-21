a9c90c23d3cade6d08b947f81aa3a4cf718d62ce
Ariel Watson <ariel.watson.chaparim@gmail.com>
2025-02-21 00:51:41 -0300

    Protótipo - mudar tema do site

---
+++ webdev/mudar-tema/script.js
@@ -0,0 +1,5 @@
+var texto = document.getElementsByClassName("nav-button");
+function Theme() {
+        document.body.style.backgroundColor = 'black';
+        document.body.getElementsByClassName("nav-button").style.color = "#ff0000";
+}
